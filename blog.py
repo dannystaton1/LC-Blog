cf84fd063cd1b8e16b75a83be5115a1d4245764c
dannystaton1 <43768074+dannystaton1@users.noreply.github.com>
2018-11-04 20:19:41 -0600

    4th Ver

--- blog.py
+++ blog.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template,session
 import cgi
 import os
 import jinja2
-from flask_sqlalchemy import SQLALchemy
+from flask_sqlalchemy import SQLAlchemy
 
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
@@ -10,5 +10,54 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), a
 
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+ pymysql://build_a_blog:9QDJJZuXsCm1UvoW@localhost:8889/build_a_blog' 
+app.config['SQLALCHEMY_ECHO']= True
 
+db = SQLAlchemy(app)
+class Post(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    content = db.Column(db.Text())
+
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+@app.route('/blog', methods=['GET'])
+def blog():
+
+    if request.args.get('post_id'):
+        post_id = request.args.get('post_id')
+        post = Post.query.get(post_id)
+
+        return render_template('post.html',title = "It's a blog!",post_title = post.title,content = post.content)
+
+    posts = Post.query.all()
+    
+    return render_template('posts.html',title="It's a Blog!",posts=posts)
+
+@app.route('/', methods=['GET','POST'])
+def newpost():
+    if request.method == 'POST':
+        title = request.form['title']
+        contents = request.form['contents']
+
+        if not title or not contents:
+            return render_template('add.html',title = "Add a Blog Entry!",error='error!')
+        
+        
+        post = Post(title, contents)
+        
+        
+        db.session.add(post)
+        db.session.commit()
+
+
+        return redirect('/blog?post_id={}'.format(post.id))
+
+    return render_template('add.html',title = "Add a Blog Entry!")
+
+if __name__ == '__main__':
+    app.run()
